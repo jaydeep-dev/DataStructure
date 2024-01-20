8a755fc6fe042313e46a6ca7c465947653e10fdb
EpicBlade <51732350+jaydeep-dev@users.noreply.github.com>
2024-01-19 21:55:22 -0500

    Setup for exercise 2 done

---
+++ Node.java
@@ -0,0 +1,47 @@
+// From repo: https://github.com/Sohaib-Mohiuddin/comp254-demo/blob/main/Week%202/Node.java
+
+public class Node {
+    private int data; // String data for Node
+    private Node next; // Reference to next Node
+
+    // Constructor
+    public Node() { }
+
+    // Constructor
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    // Getters and Setters
+    public int getData() {
+        return this.data;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    // toString method
+    public String toString() {
+        return this.data + "";
+    }
+
+    // equals method
+    public boolean equals(Node node) {
+        return this.data == node.getData();
+    }
+}
